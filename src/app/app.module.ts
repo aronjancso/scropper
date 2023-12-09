3bdd2e8ecc686874f3ed2e866e00f9504f2181dd
Aron Jancso <aron.jancso@flowject.hu>
2023-12-09 22:35:35 +0100

    Welcome aboard Angular Material!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Local modules
 import { AppRoutingModule } from './app-routing.module';
+
+// Angular Material
+import { MatIconModule } from '@angular/material/icon';
+
+// Components
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // Angular Material
+    MatIconModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
